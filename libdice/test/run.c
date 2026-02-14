983342139d7253a9af1d86ef67ac7df297c8cab9
dxdxffgg99@gmail.com <dxdxffgg99@gmail.com>
2026-02-14 10:50:22 +0900

    테스트 코드 재작성

--- libdice/test/run.c
+++ libdice/test/run.c
@@ -50,41 +50,71 @@ static E_LIBDICE_CTX_t	__puts(
 
 int main(void) {
 	libdice_ctx	CONTEXT;
-	libdice_word_t	PROGRAMME[] = {
+	libdice_word_t PROGRAMME[] = {
+		/* NOP */
 		LIBDICE_OPCODE_NOP,
-		/** eq 1, 0, 0 */
-		LIBDICE_OPCODE_EQ, 1, 0, 0, 0, 0,
-		/** puti *1 ; expected 1 */
+
+		/* Unary operations */
+		LIBDICE_OPCODE_SET, 0, 5,
+		LIBDICE_OPCODE_BNOT, 1, 1, 0,
 		LIBDICE_OPCODE_PUTI, 1, 1,
-		/** putc '\n' */
 		LIBDICE_OPCODE_PUTC, 0, '\n',
-		/** set 1, 3 */
-		LIBDICE_OPCODE_SET, 1, 3,
-		/** set 2, 3 */
-		LIBDICE_OPCODE_SET, 2, 3,
 
-		/** bxor 3, *1, *2 */
-		LIBDICE_OPCODE_BXOR, 3, 1, 1, 1, 2,
-		/** puti *3 */
-		LIBDICE_OPCODE_PUTI, 1, 3,
-
-		/** band 3, *1, *2 */
-		LIBDICE_OPCODE_BAND, 3, 1, 1, 1, 2,
-		/** puti *3 */
-		LIBDICE_OPCODE_PUTI, 1, 3,
-
-		/** putc '\n' */
+		LIBDICE_OPCODE_LNOT, 2, 1, 0,
+		LIBDICE_OPCODE_PUTI, 1, 2,
 		LIBDICE_OPCODE_PUTC, 0, '\n',
-		LIBDICE_OPCODE_JMPZA, 0, 0, 0, 6,
 
-		/** now this is skipped */
-		LIBDICE_OPCODE_EOP,
-		/** ============================= */
+		LIBDICE_OPCODE_INEG, 3, 1, 0,
+		LIBDICE_OPCODE_PUTI, 1, 3,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
 
-		/** set 0, ???; "hi" as uint32 */
-		LIBDICE_OPCODE_SET, 0, (u32_least)'h' | ((u32_least)'i' << 8),
-		/** puts 0 */
-		LIBDICE_OPCODE_PUTS, 0, 0,
+		LIBDICE_OPCODE_ITOF, 4, 1, 0,
+		LIBDICE_OPCODE_PUTF, 1, 4,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_FTOI, 5, 1, 4,
+		LIBDICE_OPCODE_PUTI, 1, 5,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_FNEG, 6, 1, 4,
+		LIBDICE_OPCODE_PUTF, 1, 6,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		/* Binary operations */
+		LIBDICE_OPCODE_SET, 1, 10,
+		LIBDICE_OPCODE_EQ, 7, 1, 0, 1, 1,
+		LIBDICE_OPCODE_PUTI, 1, 7,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_NEQ, 8, 1, 0, 1, 1,
+		LIBDICE_OPCODE_PUTI, 1, 8,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_BAND, 9, 1, 0, 1, 1,
+		LIBDICE_OPCODE_PUTI, 1, 9,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_BOR, 10, 1, 0, 1, 1,
+		LIBDICE_OPCODE_PUTI, 1, 10,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_FADD, 11, 1, 4, 1, 4,
+		LIBDICE_OPCODE_PUTF, 1, 11,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_FSUB, 12, 1, 4, 1, 4,
+		LIBDICE_OPCODE_PUTF, 1, 12,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_FMUL, 13, 1, 4, 1, 4,
+		LIBDICE_OPCODE_PUTF, 1, 13,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		LIBDICE_OPCODE_FDIV, 14, 1, 4, 1, 4,
+		LIBDICE_OPCODE_PUTF, 1, 14,
+		LIBDICE_OPCODE_PUTC, 0, '\n',
+
+		/* End of program */
 		LIBDICE_OPCODE_EOP,
 	};
 	libdice_put_interface	INTERFACE;
@@ -114,7 +144,7 @@ int main(void) {
 			, LOOKUP, sizeof(LOOKUP) / sizeof(LOOKUP[0])
 			, &LCK);
 
-	printf("Final State: %u\n", CONTEXT.m_state);
+	printf("\nFinal State: %u\n", CONTEXT.m_state);
 	printf("Programme counter: %u\n", CONTEXT.m_pc);
 
 	return (int)CONTEXT.m_state;
